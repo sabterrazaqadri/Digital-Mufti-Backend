1f205f3a4168e03e82ec72324f957a79e681b175
Sabter Iqbal <sabterrazaqadri@gmail.com>
2025-12-24 23:56:12 +0500

    Update GenerativeModel to use gemini-3-flash-preview

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if not api_key:
     raise ValueError("❌ GEMINI_API_KEY missing in .env")
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-3-flash-preview")
 
 # Initialize FastAPI app
 app = FastAPI()
